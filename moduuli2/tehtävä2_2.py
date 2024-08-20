945c10aac7d8311761bed7910fca163bdc75519b
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-08-20 17:21:48 +0300

    tehtävä 2_2

--- moduuli2/tehtävä2_2.py
+++ moduuli2/tehtävä2_2.py
@@ -1,4 +1,7 @@
+import math
+
 sade_str = input('Anna ympyran sade: ')
 sade = float(sade_str)
-pintaala = (sade ** 2) * 3.14
+pintaala = (sade ** 2) * math.pi
+pintaala = round(pintaala, 2)
 print("Ympyran pinta-ala: " + str(pintaala))

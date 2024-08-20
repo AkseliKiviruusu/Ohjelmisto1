8c754438ecd512cca6e929a61a886d79de97fb90
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-08-20 17:08:09 +0300

    tehtävä 2_4

--- moduuli2/tehtävä2_4.py
+++ moduuli2/tehtävä2_4.py
@@ -1,10 +1,10 @@
 print("Anna 3 satunnaista kokonaislukua: ")
-luku_1_str = input("Luku1: ")
-luku_2_str = input("Luku2: ")
-luku_3_str = input("Luku3: ")
+luku1_str = input("Luku1: ")
+luku2_str = input("Luku2: ")
+luku3_str = input("Luku3: ")
 
-summa = float(luku_1_str) + float(luku_2_str) + float(luku_3_str)
-tulo = float(luku_1_str) * float(luku_2_str) * float(luku_3_str)
+summa = float(luku1_str) + float(luku2_str) + float(luku3_str)
+tulo = float(luku1_str) * float(luku2_str) * float(luku3_str)
 keskiarvo = float(summa) //3
 
 print("Lukujen summa:", str(summa))

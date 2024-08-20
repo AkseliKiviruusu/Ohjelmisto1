dbf6db681ad164bd6292bf3ecef6899b5e0cd6eb
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-08-20 20:28:02 +0300

    tehtävä 2_5

---
+++ moduuli2/tehtävä2_6.py
@@ -0,0 +1,12 @@
+import random
+
+print ("Kolmenumeroinen koodi:",
+       random.randint(0,9),
+       random.randint(0,9),
+       random.randint(0,9))
+
+print ("Neliumeroinen koodi:",
+       random.randint(1,6),
+       random.randint(1,6),
+       random.randint(1,6),
+       random.randint(1,6))

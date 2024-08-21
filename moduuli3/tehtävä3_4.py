c6aff032ec7898226ed56affb2bca5f4a6295e90
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-08-21 14:45:25 +0300

    tehtävä 3_4

--- moduuli3/tehtävä3_4.py
+++ moduuli3/tehtävä3_4.py
@@ -1,9 +1,9 @@
 vuosi = int(input("Anna vuosiluku: "))
 
 if ((vuosi % 100 == 0 and vuosi % 400 == 0)
-        or vuosi % 4 == 0
-        and (vuosi % 100) % 400 > 0)\
-        and vuosi != 0:
+    or vuosi % 4 == 0
+    and (vuosi % 100) % 400 > 0) \
+    and vuosi != 0:
     print("Vuosi on karkausvuosi")
 else:
     print("Vuosi ei ole karkausvuosi")

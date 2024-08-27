bf3784dcbe52cdbe757815ceb0805d42478a879a
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-08-27 10:52:48 +0300

    tehtävä 4_2

---
+++ moduuli4/tehtävä4_2.py
@@ -0,0 +1,5 @@
+tuumat = float(input("Anna mitta tuumina: "))
+while tuumat > 0:
+    print("Sama mitta sentteinä: " + str(tuumat * 2.54))
+    tuumat = float(input("Anna mitta tuumina: "))
+print("Toiminnot lopetettu.")

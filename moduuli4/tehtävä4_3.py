68914cd062c9467465b140f4a9f747939265c898
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-08-27 13:46:25 +0300

    tehtävä 4_3

--- moduuli4/tehtävä4_3.py
+++ moduuli4/tehtävä4_3.py
@@ -1,16 +1,16 @@
 luku = float(input("Anna luku: "))
-suurin_luku = luku
-pienin_luku = luku
+suurin = luku
+pienin = luku
 
-while str(luku) != str():
+while str(luku) != "":
     luku = input("Anna luku: ")
     if luku != "":
-        if suurin_luku < float(luku):
-            suurin_luku = float(luku)
-        else: suurin_luku = suurin_luku
-        if pienin_luku > float(luku):
-            pienin_luku = float(luku)
-        else: pienin_luku = pienin_luku
+        if suurin < float(luku):
+            suurin = float(luku)
+        else: suurin = suurin
+        if pienin > float(luku):
+            pienin = float(luku)
+        else: pienin = pienin
 
-print(f"Pienin antamasi luku: {pienin_luku}")
-print(f"Suurin antamasi luku: {suurin_luku}")
+print(f"Pienin antamasi luku: {pienin}")
+print(f"Suurin antamasi luku: {suurin}")

522c5556beeff57a50a9a806587a0f10ebda6597
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-08-28 16:55:26 +0300

    tehtävä 4_6

--- moduuli4/tehtävä4_6.py
+++ moduuli4/tehtävä4_6.py
@@ -6,9 +6,9 @@ sisalla = 0
 
 while kerrat < pisteet:
     kerrat = kerrat + 1
-    piste_1 = random.uniform(-1, 1)
-    piste_2 = random.uniform(-1, 1)
-    if piste_1 ** 2 + piste_2 ** 2 < 1:
+    p_1 = random.uniform(-1, 1)
+    p_2 = random.uniform(-1, 1)
+    if p_1 ** 2 + p_2 ** 2 < 1:
         sisalla = sisalla + 1
 
 pi = 4 * (sisalla / pisteet)

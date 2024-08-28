afbdb9f40b048dc7ea53adb059470c8a5136e28d
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-08-28 20:36:07 +0300

    tehtävä 5_3

--- moduuli5/tehtävä5_3.py
+++ moduuli5/tehtävä5_3.py
@@ -1,15 +1,18 @@
-luku = int(input("Anna luku: "))
-on_alkuluku = False
+import math
 
-if (luku % 2 == 0) or (luku == 0) or (luku == 1):
+luku = int(input("Anna luku: "))
+
+if luku < 2:
     print("Ei ole alkuluku")
 elif luku == 2:
-    print("On alkuluku")
-else:
-    for i in range(2,luku):
-        if luku % i != 0:
-            on_alkuluku = True
-if on_alkuluku == True:
-    print("On alkuluku")
-else:
     print("Ei ole alkuluku")
+else:
+    on_alkuluku = True
+    for i in range(2, int(math.sqrt(luku)) + 1):
+        if luku % i == 0:
+            on_alkuluku = False
+            break
+    if on_alkuluku:
+        print("On alkuluku")
+    else:
+        print("Ei ole alkuluku")

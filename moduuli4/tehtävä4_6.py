96ade72a478cd52c643eb018a99e9ca119d32f08
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-08-28 16:53:23 +0300

    tehtävä 5_2

--- moduuli4/tehtävä4_6.py
+++ moduuli4/tehtävä4_6.py
@@ -1,8 +1,4 @@
-import math
 import random
-
-NELIO = 2 * 2
-YMPYRA = (1 ** 2) * math.pi
 
 pisteet = int(input("Anna neliön sisälle piirrettävien pisteiden määrä: "))
 kerrat = 0
@@ -10,10 +6,10 @@ sisalla = 0
 
 while kerrat < pisteet:
     kerrat = kerrat + 1
-    piste = random.uniform(0, 4)
-    if piste < YMPYRA:
+    piste_1 = random.uniform(-1, 1)
+    piste_2 = random.uniform(-1, 1)
+    if piste_1 ** 2 + piste_2 ** 2 < 1:
         sisalla = sisalla + 1
 
 pi = 4 * (sisalla / pisteet)
-print()
-print(f"pi likiarvo: {pi}")
+print(pi)

903d6ef6a0aac08d57ee37bc6a43603611051fc1
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-09-03 10:09:04 +0300

    tehtävä 5_4

--- moduuli5/tehtävä5_4.py
+++ moduuli5/tehtävä5_4.py
@@ -1,6 +1,8 @@
 kaupungit = []
+kerrat = 0
 
-for i in range(5):
+while kerrat < 5:
+    kerrat = kerrat + 1
     kaupungit.append(input("Anna satunnaisen kaupungin nimi: "))
 
 for kaupunki in kaupungit:

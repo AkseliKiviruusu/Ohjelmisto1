221a15fd7de1f29d2d3e666914a53e18da02be64
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-09-03 10:10:40 +0300

    tehtävä 5_4

--- moduuli5/tehtävä5_4.py
+++ moduuli5/tehtävä5_4.py
@@ -1,6 +1,8 @@
 kaupungit = []
 kerrat = 0
 
+# tehtävänannossa "for", luennolla sanottiin käyttää "while" komentoa.
+
 while kerrat < 5:
     kerrat = kerrat + 1
     kaupungit.append(input("Anna satunnaisen kaupungin nimi: "))

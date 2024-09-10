fde5d4f9fc553261ae6a92145274bd8dd2e8d39f
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-09-10 14:24:33 +0300

    tehtävä 8_1

---
+++ moduuli8/tehtävä8_1.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+def get_airport_by_icao(icao):
+    sql = f"SELECT name, municipality FROM airport WHERE ident = %s"
+    kursori = yhteys.cursor()
+    kursori.execute(sql, (icao, ))
+    airport_data = kursori.fetchall()
+    return airport_data
+
+yhteys = mysql.connector.connect(
+         host = '127.0.0.1',
+         port = 3306,
+         database = 'flight_game',
+         user = 'akselikiviruusu',
+         password = 'Rm4g9090?',
+         autocommit = True
+         )
+
+name_municipality = get_airport_by_icao(input("Anna lentoaseman ICAO-koodi: "))
+for i in name_municipality:
+    print(f"Lentoaseman nimi: \n{i[0]}")
+    print(f"Sijaintikunta: \n{i[1]}")
